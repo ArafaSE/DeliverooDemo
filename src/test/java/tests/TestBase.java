c4f87b00aa0b3e7ade8a5799508b1df4413d9978
ArafaSE <mohamed.arafa@bosta.co>
2022-07-07 20:40:18 +0200

    Remove old chrome driver implementation and use Driver manager instead

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,39 +17,6 @@ public class TestBase extends AbstractTestNGCucumberTests {
     public static WebDriver driver;
     public  String HOME_URL = "https://deliveroo.co.uk/";
 
-//    @BeforeSuite
-//    @Parameters({"browser"})
-//    public void startDriver(@Optional("chrome") String browserName) {
-//        if (browserName.equalsIgnoreCase("chrome")) {
-//            System.setProperty("webdriver.chrome.driver",
-//                    System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
-//            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-//            driver = new ChromeDriver();
-//
-//            driver.manage().window().maximize();
-//            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-//        }
-//        // headless browser testing with Chrome headless option
-//        else if (browserName.equalsIgnoreCase("chrome-headless")) {
-//            System.setProperty("webdriver.chrome.driver",
-//                    System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
-//            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-//
-//            ChromeOptions option = new ChromeOptions();
-//            option.addArguments("--headless");
-//            option.addArguments("--disable-gpu");
-//            option.addArguments("--window-size=1920,1080");
-//            option.addArguments("--disable-extensions");
-//            option.addArguments("--start-maximized");
-//            option.addArguments("--no-sandbox");
-//            option.addArguments("--ignore-certificate-errors");
-//            option.addArguments("--allow-running-insecure-content");
-//            driver = new ChromeDriver(option);
-//        }
-//
-//        driver.navigate().to(HOME_URL);
-//    }
-
     @BeforeSuite
     @Parameters({"browser"})
     public void startDriver(@Optional("chrome") String browserName) {
